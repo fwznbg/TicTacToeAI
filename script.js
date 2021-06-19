7f891df7ed6e5aa5e84136a7b32e8a967874ccf3
fwznbg <60412952+fwznbg@users.noreply.github.com>
2021-06-19 23:54:17 +0700

    AI v1

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+// initialization
 let player = "";
 let computer = "";
+let board_full = false;
+let board = ["", "", "", "", "", "", "", "", ""];
+
+/*
+  Game pre-start
+*/
 
 const choose = document.querySelector(".choose");
+const game = document.querySelector(".game");
+const winner_statement = document.querySelector(".winner--container");
 
 const choose_side = (side) => {
   if(side == "X"){
@@ -10,17 +19,13 @@ const choose_side = (side) => {
   }else{
     player = "O";
     computer = "X";
+<<<<<<< Updated upstream
+=======
+    addComputerMove();
+>>>>>>> Stashed changes
   }
   choose.style.visibility = "hidden";	
 }
-
-
-let board_full = false;
-let board = ["", "", "", "", "", "", "", "", ""];
-
-const game = document.querySelector(".game");
-
-const winner_statement = document.querySelector(".winner--container");
 
 check_board_complete = () => {
   let flag = true;
@@ -43,25 +48,45 @@ const check_line = (a, b, c) => {
 const check_match = () => {
   for (i = 0; i < 9; i += 3) {
     if (check_line(i, i + 1, i + 2)) {
-      return board[i];
+      match = {
+        winner: board[i],
+        score: 0
+      } 
+      match["score"] = (board[i] == player) ? +10 : (board[i] == player) ? -10: 0;
+      return match;
     }
   }
   for (i = 0; i < 3; i++) {
     if (check_line(i, i + 3, i + 6)) {
-      return board[i];
+      match = {
+        winner: board[i],
+        score: 0
+      } 
+      match["score"] = (board[i] == player) ? +10 : (board[i] == player) ? -10: 0;
+      return match;
     }
   }
   if (check_line(0, 4, 8)) {
-    return board[0];
+    match = {
+      winner: board[0],
+      score: 0
+    } 
+    match["score"] = (board[0] == player) ? +10 : (board[0] == player) ? -10: 0;
+    return match;
   }
   if (check_line(2, 4, 6)) {
-    return board[2];
+    match = {
+      winner: board[2],
+      score: 0
+    } 
+    match["score"] = (board[2] == player) ? +10 : (board[2] == player) ? -10: 0;
+    return match;
   }
   return "";
 };
 
 const check_for_winner = () => {
-  let res = check_match()
+  let res = check_match()["winner"];
   if (res == player) {
     winner.innerText = "You Win!";
     board_full = true
@@ -93,32 +118,6 @@ const game_loop = () => {
   check_for_winner();
 }
 
-const addPlayerMove = e => {
-  if (!board_full && board[e] == "" && player != "") {
-    board[e] = player;
-    game_loop();
-    addComputerMove(false);
-  }
-};
-
-const addComputerMove = (isSmart) => {
-  isSmart? SmartMove() : RandomMove();
-};
-
-const RandomMove = () => {
-  if (!board_full) {
-    do {
-      selected = Math.floor(Math.random() * 9);
-    } while (board[selected] != "");
-    board[selected] = computer;
-    game_loop();
-  }
-}
-
-const SmartMove = () => {
-
-}
-
 const reset_board = () => {
   player = "";
   computer = "";
@@ -135,5 +134,77 @@ const x_button = () => {
   document.getElementById("hidden--button").style.visibility = "visible";
 }
 
-//initial render
+/* Game's move */
+
+const addPlayerMove = e => {
+  if (!board_full && board[e] == "" && player != "") {
+    board[e] = player;
+    game_loop();
+    addComputerMove();
+  }
+};
+
+const minimaxMove = (board, depth, isMax) => {
+  let score = check_match()["score"];
+
+  if(score == 10) return score;
+  if(score == -10) return score;
+  if(board_full) return 0;
+
+  if(isMax){
+    let bestMove = -Infinity;
+    for(let i = 0; i<9;i++){
+      if(board[i]==""){
+        board[i] = player;
+        bestMove = Math.max(bestMove, minimaxMove(board, depth+1, !isMax));
+
+        board[i] = "";
+      }
+    }
+    return bestMove;
+  }else{
+    let bestMove = Infinity;
+    for(let i = 0; i<9;i++){
+      if(board[i]==""){
+        board[i] = computer;
+        bestMove = Math.min(bestMove, minimaxMove(board, depth+1, !isMax));
+
+        board[i] = "";
+      }
+    }
+    return bestMove;
+  }
+};
+
+const findComputerMove = () => {
+  let best = Infinity;
+  let bestMove = -1;
+
+  for(let i = 0; i<9;i++){
+    if(board[i]==""){
+      board[i] = computer;
+      let move = minimaxMove(board, 0, false);
+      board[i] = "";
+      
+      if(move<best) {
+        bestMove = i;
+        best = move;
+      }
+    }
+  }
+  return bestMove;
+}
+
+const addComputerMove = () => {
+  if (!board_full) {
+    // do {
+    //   selected = Math.floor(Math.random() * 9);
+    // } while (board[selected] != "");
+    board[findComputerMove()] = computer;
+    game_loop();
+  }
+}
+
+
+// render
 render_board();
